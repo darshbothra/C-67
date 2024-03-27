df0ecb780b6c33e536e8063bfbc23f922fa9448a
darshbothra <87893198+darshbothra@users.noreply.github.com>
2024-03-27 22:56:22 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+// add SDK Firebase
+var firebaseConfig = {
+    const firebaseConfig = {
+        apiKey: "AIzaSyCVIfv9-YqxmtmtYXSymm819CDX23_J6ZM",
+        authDomain: "c-67-73f58.firebaseapp.com",
+        projectId: "c-67-73f58",
+        storageBucket: "c-67-73f58.appspot.com",
+        messagingSenderId: "665430807914",
+        appId: "1:665430807914:web:e3ec8eecde47a8d8a88113"
+      };
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.database();
